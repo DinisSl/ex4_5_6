a206eb8853702e0bbe03f7155109e66339d4f28a
user <dinissantanalopes@gmail.com>
2026-03-13 11:37:59 +0000

    cenas

---
+++ src/components/Slideshow.jsx
@@ -0,0 +1,13 @@
+import slide1 from "../assets/images/slides-1.png";
+
+const Slideshow = () => {
+    return (
+        <div id="box0">
+            <div id="slideShow">
+                <img src={slide1} alt="img"/>
+            </div>
+        </div>
+    )
+}
+
+export default Slideshow;
